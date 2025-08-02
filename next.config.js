0edd490fa6a46df0a665ed1c9bbc0da9b06bc707
ItsMeGingerGun <happysolutionsivakasi@gmail.com>
2025-08-02 22:51:38 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ["canvas", "ioredis"],
+    serverComponentsExternalPackages: ["ioredis"],
   },
   images: {
     remotePatterns: [
