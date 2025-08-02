459eb0b6f042c8656c4131afbe810bd678776ef9
ItsMeGingerGun <happysolutionsivakasi@gmail.com>
2025-08-02 21:23:10 +0530

    Update redis.ts

--- src/app/lib/redis.ts
+++ src/app/lib/redis.ts
@@ -10,7 +10,18 @@ export const getRedisClient = () => {
   if (!url) {
     throw new Error('REDIS_URL environment variable not set');
   }
+// Add these to your redis.ts file
+export const getPublicTemplates = async () => {
+  const client = getRedisClient();
+  const publicTemplates = await client.zrange('public_templates', 0, -1);
+  return Promise.all(publicTemplates.map(id => getTemplate(id)));
+};
 
+export const getUserTemplates = async (fid: number) => {
+  const client = getRedisClient();
+  const userTemplates = await client.smembers(`user:${fid}:templates`);
+  return Promise.all(userTemplates.map(id => getTemplate(id)));
+};
   redisClient = new Redis(url, {
     tls: {
       rejectUnauthorized: false
