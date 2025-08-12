82b209aaeaebf035a9dc6a52d8c96bcc9f6c6fce
ItsMeGingerGun <happysolutionsivakasi@gmail.com>
2025-08-12 22:59:05 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,22 +5,14 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-      },
       animation: {
         fadeIn: 'fadeIn 0.5s ease-out forwards',
-        pulse: 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
         blob: 'blob 7s infinite',
       },
       keyframes: {
         fadeIn: {
           '0%': { opacity: 0, transform: 'translateY(10px)' },
           '100%': { opacity: 1, transform: 'translateY(0)' }
-        },
-        pulse: {
-          '0%, 100%': { opacity: 1 },
-          '50%': { opacity: 0.5 }
         },
         blob: {
           '0%': { transform: 'translate(0px, 0px) scale(1)' },
