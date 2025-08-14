98461be49364df5713e10c52364e74b54eed0188
ItsMeGingerGun <happysolutionsivakasi@gmail.com>
2025-08-14 16:38:02 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
       animation: {
         fadeIn: 'fadeIn 0.5s ease-out forwards',
         blob: 'blob 7s infinite',
+        spin: 'spin 1s linear infinite',
       },
       keyframes: {
         fadeIn: {
@@ -19,10 +20,19 @@ module.exports = {
           '33%': { transform: 'translate(30px, -50px) scale(1.1)' },
           '66%': { transform: 'translate(-20px, 20px) scale(0.9)' },
           '100%': { transform: 'translate(0px, 0px) scale(1)' }
+        },
+        spin: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' }
         }
       },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      },
+      zIndex: {
+        '10': '10',
+        '9999': '9999',
+        '-1': '-1',
       }
     },
   },
